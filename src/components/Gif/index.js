f1def22a8d72501a6f9574cc0a4c848c4dc6c186
EnGkong-Dev <randyrf@student.gunadarma.ac.id>
2022-03-26 18:17:04 +0700

    gigih-homework-5

---
+++ src/components/Gif/index.js
@@ -0,0 +1,32 @@
+// import SearchBar from '../Search'
+import gif from '../../store/gif'
+// import ButtonElement from '../Buttons'
+
+function Gif(){
+    return(
+        <>
+            <div>
+                <div>
+                    {/* <SearchBar/> */}
+                    {/* <ButtonElement/> */}
+                </div>
+                    <div style={{display: "flex"}}>
+                        {gif.map( e => {
+                            return(
+                                <div style={{padding: "40px"}} key={e.id}>
+                                    <img src={e.url} alt={e.title}/><br />
+                                    <span style={{backgroundColor: "yellow", padding: "10px"}}>{e.rating}</span>
+                                    <p>{e.title}</p>
+                                </div>
+                                )
+
+                            })
+                        }
+                    </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Gif
